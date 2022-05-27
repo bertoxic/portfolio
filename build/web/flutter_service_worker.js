b444931ab6a1ffb7411231b36e1bb96d3ef2383d
Bertoxic <dirusalbert@gmail.com>
2022-05-27 11:38:04 -0700

    new

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -60,9 +60,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "5351664709b596756cefeaf5dd3e95c7",
-"/": "5351664709b596756cefeaf5dd3e95c7",
-"main.dart.js": "afec5d4a4eb1c0e592f42e468e48e714",
+"index.html": "1869418e105ddfb9684d4cce31a56208",
+"/": "1869418e105ddfb9684d4cce31a56208",
+"main.dart.js": "d4d281689ededacff4698f1a3400a542",
 "manifest.json": "9432f3169231df869a380175565e0ecc",
 "version.json": "9094aacdae789dccd67fa32109ff1a18"
 };
