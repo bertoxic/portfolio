c1825ab6bcc6a30663e5e482c9434ad869e4ba5d
Bertoxic <dirusalbert@gmail.com>
2022-05-27 14:39:32 -0700

    err

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -60,9 +60,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a15203aaa48f6c7a4487cce1aa36c7d5",
-"/": "a15203aaa48f6c7a4487cce1aa36c7d5",
-"main.dart.js": "40cc5a6d70b6deeea472a9efe2993f96",
+"index.html": "a7569488ab39c2efaebdb1f50e74f8c7",
+"/": "a7569488ab39c2efaebdb1f50e74f8c7",
+"main.dart.js": "12ff008a6d397f34b2f340ab6bd6e144",
 "manifest.json": "9432f3169231df869a380175565e0ecc",
 "version.json": "9094aacdae789dccd67fa32109ff1a18"
 };
